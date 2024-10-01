482863cc384ff3dc4b1dfd85ef108bd42a3a85b6
dautroc1 <51069344+dautroc1@users.noreply.github.com>
2024-10-01 09:52:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,17 +19,7 @@ def ping_website():
 def ping_api():
     try:
         # Send a GET request to the website
-        response = requests.get('https://gpt-wxjs.onrender.com')
-        response = requests.get('https://gpt1-oj96.onrender.com')
-        response = requests.get('https://gpt2-brtm.onrender.com')
-        response = requests.get('https://gpt3-8vfp.onrender.com')
-        response = requests.get('https://gpt4-fxzq.onrender.com')
-        response = requests.get('https://gpt4-08a7.onrender.com')
         response = requests.get('https://test-gpt-y58q.onrender.com')
-        response = requests.get('https://www.downloadvideos.click/')
-        response = requests.get('https://audio-split-back-1.onrender.com')
-        response = requests.get('https://audio-split-back.onrender.com')
-        response = requests.get('https://audio-split-back-2.onrender.com')
         return "Website pinged successfully", 200
         
     except Exception as e:
